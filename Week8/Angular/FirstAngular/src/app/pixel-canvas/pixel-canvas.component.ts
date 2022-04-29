6f8b8e870ee354b586ac2fab7f67743c26b6a91e
anil villamkandathil <vnanil@gmail.com>
2022-04-29 16:59:17 -0400

    Adding Angular

---
+++ Week8/Angular/FirstAngular/src/app/pixel-canvas/pixel-canvas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pixel-canvas',
+  templateUrl: './pixel-canvas.component.html',
+  styleUrls: ['./pixel-canvas.component.css']
+})
+export class PixelCanvasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
